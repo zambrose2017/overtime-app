4efb4060bc93e850315f638df426d046052f3e09
Zac Ambrose <zambrose2017@gmail.com>
2017-03-22 15:45:24 -0600

    implemented new styling to list users by full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         validates_presence_of :first_name, :last_name
+  validates_presence_of :first_name, :last_name
+
+  def full_name
+    last_name.upcase + ", " + first_name.upcase
+  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,51 +1,55 @@
 require 'rails_helper'
+
 describe 'navigate' do
-  before do 
-    user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      login_as(user, :scope => :user)
-    end
-  describe "index" do 
+  before do
+    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(@user, :scope => :user)
+  end
+
+  describe 'index' do
     before do
-       visit posts_path
+      visit posts_path
     end
-    it "can be reached successfully" do 
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
-  it "has a title of Posts" do 
+
+    it 'has a title of Posts' do
       expect(page).to have_content(/Posts/)
     end
 
-    it "has a list of posts" do 
-      post1 = Post.create(date: Date.today, rationale: "Post 1", user_id: @user.id)
-      post2 = Post.create(date: Date.today, rationale: "Post 2", user_id: @user.id)
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
       visit posts_path
       expect(page).to have_content(/Post1|Post2/)
     end
   end
 
-  describe "creation" do 
+  describe 'creation' do
     before do
       visit new_post_path
     end
-    
-    it "has a new form that can be reached" do 
+
+    it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
 
-    it "can be breated from a new form page" do 
-      fill_in "post[date]", with: Date.today
-      fill_in "post[rationale]", with: "Some rationale"
-
+    it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
     end
-    it "will have a user associated with it" do
-      fill_in "post[date]", with: Date.today
-      fill_in "post[rationale]", with: "User Association"
 
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
+
       expect(User.last.posts.last.rationale).to eq("User Association")
-    end 
-  end    
-end  
+    end
+  end
+end

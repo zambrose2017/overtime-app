ee7a5930ed02b95a2189e09b050c66a62b49d13c
Zac Ambrose <zambrose2017@gmail.com>
2017-03-22 12:11:58 -0600

    Implemented User connection to posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe "Creation" do 
-    before do 
-      @post = Post.create(date: Date.today, rationale: "anything")
+  describe "Creation" do
+    before do
+      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Smith")
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: user.id)
     end
-    it "can be created" do 
+
+    it "can be created" do
+      
       expect(@post).to be_valid
-     end 
-    it "cannot be created without a date and rationale" do 
+    end
+    it "cannot be created without datee, rationale" do
       @post.date = nil
       @post.rationale = nil
       expect(@post).to_not be_valid

60fe34be51389ce805718b5cdad961d90c143d20
Zac Ambrose <zambrose2017@gmail.com>
2017-03-22 22:02:26 -0600

    refactored features, factories and models using FactoryGirl

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,20 @@
 FactoryGirl.define do
+  sequence :email do |n|
+    "test#{n}@example.com"
+  end
+
   factory :user do
-    first_name "Jon"
-    last_name "Snow"
-    email "test@test.com"
+    first_name 'Jon'
+    last_name 'Snow'
+    email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
   end
 
-  factory :admin_user, class: "AdminUser"do
-    first_name "Admin"
-    last_name "User"
-    email "admin@tuser.com"
+  factory :admin_user, class: "AdminUser" do
+    first_name 'Admin'
+    last_name 'User'
+    email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
   end

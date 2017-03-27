e3a0391c90126978dd596ee8b6483e643ddc289f
Zac Ambrose <zambrose2017@gmail.com>
2017-03-27 14:19:36 -0600

    customized searchability in the admin dashboard

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -8,12 +8,13 @@ class PostDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    user: Field::BelongsTo,
-    id: Field::Number,
-    date: Field::DateTime,
-    rationale: Field::Text,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    user: Field::BelongsTo.with_options(searchable: false),
+    id: Field::Number.with_options(searchable: false),
+    date: Field::DateTime.with_options(searchable: false),
+    work_performed: Field::Text.with_options(searchable: true),
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
+    status: Field::Text.with_options(searchable: true),
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -23,18 +24,19 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :user,
-    :id,
+    :status,
     :date,
-    :rationale,
+    :work_performed,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :user,
+    :status,
     :id,
     :date,
-    :rationale,
+    :work_performed,
     :created_at,
     :updated_at,
   ].freeze
@@ -45,7 +47,7 @@ class PostDashboard < Administrate::BaseDashboard
   FORM_ATTRIBUTES = [
     :user,
     :date,
-    :rationale,
+    :work_performed,
   ].freeze
 
   # Overwrite this method to customize how posts are displayed

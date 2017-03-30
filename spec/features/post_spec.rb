dd9bd7cf021d05f2be0ae0b4f9e1352d3f1babb0
Zac Ambrose <zambrose2017@gmail.com>
2017-03-30 15:36:09 -0600

    Added hours requested to post model

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe 'navigate' do
-    let(:user) {FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user) }
 
-    let(:post) do 
-      Post.create(date: Date.today, rationale: "Rationale", user_id: user.id)
+  let(:post) do
+    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id, overtime_request: 3.5)
   end
+
   before do
     login_as(user, :scope => :user)
   end
@@ -30,10 +31,10 @@ describe 'navigate' do
       expect(page).to have_content(/Rationale|content/)
     end
 
-    it "has a scope so that only post creators can see their posts" do
-      other_user = User.create(first_name: 'Non',  last_name: 'Authorized', email: 'nonauth@test.com', password: "asdfasdf", password_confirmation: "asdfasdf")
-      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
-      
+    it 'has a scope so that only post creators can see their posts' do
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id, overtime_request: 3.5)
+
       visit posts_path
 
       expect(page).to_not have_content(/This post shouldn't be seen/)
@@ -56,7 +57,7 @@ describe 'navigate' do
       delete_user = FactoryGirl.create(:user)
       login_as(delete_user, :scope => :user)
 
-      post_to_delete = Post.create(date: Date.today, rationale: "stuff", user_id: delete_user.id)
+      post_to_delete = Post.create(date: Date.today, rationale: 'asdf', user_id: delete_user.id, overtime_request: 3.5)
 
       visit posts_path
 
@@ -77,14 +78,15 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-      click_on "Save"
+      fill_in 'post[overtime_request]', with: 4.5
 
-      expect(page).to have_content("Some rationale")
+      expect { click_on "Save" }.to change(Post, :count).by(1)
     end
 
     it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
+      fill_in 'post[overtime_request]', with: 4.5
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
@@ -108,6 +110,7 @@ describe 'navigate' do
       login_as(non_authorized_user, :scope => :user)
 
       visit edit_post_path(post)
+
       expect(current_path).to eq(root_path)
     end
   end

c4d5f079ca6faf214e5c40eff3f43961123ed4f0
Zac Ambrose <zambrose2017@gmail.com>
2017-04-06 14:51:41 -0600

    brought in live data for review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,10 @@ AdminUser.create(email: "admin@test.com",
 puts ("1 Admin user created")
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sioccaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
  end
+
+puts "100 audit logs have been created"
 
  100.times do |audit_log|
   AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))

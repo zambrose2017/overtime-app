24f001d40d2f761d210fa571a4081e3136f2f44d
Zac Ambrose <zambrose2017@gmail.com>
2017-04-10 12:03:49 -0600

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,30 @@
-@user = User.create(email: "test@test.com",
-                    password: "asdfasdf", 
-                    password_confirmation: "asdfasdf", 
-                    first_name: "Jon", 
+@employee = Employee.create(email: "test@test.com",
+                    password: "asdfasdf",
+                    password_confirmation: "asdfasdf",
+                    first_name: "Jon",
                     last_name: "Snow",
-                    phone: "2083140933")
+                    phone: "4322386131")
 
-puts ("1 user created")
+puts "1 employee created"
 
-AdminUser.create(email: "admin@test.com", 
-                password: "asdfasdf", 
-                password_confirmation: "asdfasdf", 
-                first_name: "Admin", 
-                last_name: "User",
-                phone: "2083140933")
+AdminUser.create(email: "admin@test.com",
+                  password: "asdfasdf",
+                  password_confirmation: "asdfasdf",
+                  first_name: "Admin",
+                  last_name: "Name",
+                  phone: "4322386131")
 
-puts ("1 Admin user created")
+puts "1 Admin user created"
 
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-   AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-   AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sioccaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
- end
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 2.5)
+end
 
-puts "100 audit logs have been created"
+puts "100 Posts have been created"
 
- 

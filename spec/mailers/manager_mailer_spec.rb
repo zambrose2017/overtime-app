f2af2bea5f0d619c8983043814d87a79373f71af
Zac Ambrose <zambrose2017@gmail.com>
2017-04-10 12:29:25 -0600

    fixed ManagerMailer

--- spec/mailers/manager_mailer_spec.rb
+++ spec/mailers/manager_mailer_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe ManagerMailerMailer, type: :mailer do
+RSpec.describe ManagerMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
 end

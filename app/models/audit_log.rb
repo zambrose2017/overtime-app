33ed412cadabbe057572a620ba89e8fc4eeba705
Zac Ambrose <zambrose2017@gmail.com>
2017-04-10 15:56:09 -0600

    refactored button functionality for amployee dash

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -6,6 +6,8 @@ class AuditLog < ActiveRecord::Base
 
   before_update :set_end_date, if: :confirmed?
 
+  scope :by_start_date, -> {order ('start_date DESC')}
+
   private
     def set_end_date
       self.end_date = Date.today

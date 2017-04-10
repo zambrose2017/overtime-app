b33d9f09d4fcba0ac3b0aba7e59fed3d8d32b5e8
Zac Ambrose <zambrose2017@gmail.com>
2017-04-10 15:26:25 -0600

    updated callback for audit logs

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -4,8 +4,12 @@ class AuditLog < ActiveRecord::Base
   validates_presence_of :user_id, :status, :start_date
   after_initialize :set_defaults
 
+  before_update :set_end_date, if: :confirmed?
 
   private
+    def set_end_date
+      self.end_date = Date.today
+    end
 
     def set_defaults
       self.start_date ||= Date.today - 6.days
